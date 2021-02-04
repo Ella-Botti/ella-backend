93a73b5b4e34236ee0f3f351a262975c936759b6
Samuli Peltonen <samuli.peltonen1@gmail.com>
2021-02-04 15:53:08 +0200

    Made newssearch.py a function

--- newssearch.py
+++ newssearch.py
@@ -1,20 +1,17 @@
 import json
 import re
-with open('./articles.json') as json_file:
-    arr = []
-    keyword1 = "'TITLE':"
-    keyword2 = ", 'PUBLISHED'"
-    data = json.load(json_file)
-    #queryString = 'Matchine queries: '
-    #query = input("Query parameters: ")
-    for i,j in data.items():
-        arr.append(str(j))
-    print("Data added to array, ready to search:")
-    queryString=input("Articles that contain the query:")
-    
-    for i in range(len(arr)):
-        if queryString in arr[i]:
-            #print(re.search("(?P<url>https?://[^\s]+)", arr[i]).group("url"))
-            result = re.search("'TITLE':(.*), 'PUBLISHED'", arr[i])
-            print(result.group(1) + "  -  " +re.search("(?P<url>https?://[^\s]+)", arr[i]).group("url") )
-            
+def search_keyword(queryString):
+    with open('./articles.json') as json_file:
+        results = []
+        filteredResults = []
+        keyword1 = "'TITLE':"
+        keyword2 = ", 'PUBLISHED'"
+        data = json.load(json_file)
+        for i,j in data.items():
+            results.append(str(j))
+        
+        for i in range(len(results)):
+            if queryString in results[i]:
+                result = re.search("'TITLE':(.*), 'PUBLISHED'", results[i])
+                filteredResults.append(result.group(1) + "  -  " +re.search("(?P<url>https?://[^\s]+)", results[i]).group("url"))
+    return(filteredResults)         
