a4c532f289746329ff7cffc002823663cac93dd7
mariia.pyylampi@gmail.com <mariia.pyylampi@gmail.com>
2021-02-11 13:28:57 +0200

    Added language option

--- TelegBotTest.py
+++ TelegBotTest.py
@@ -17,6 +17,33 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+def language(update: Update, context: CallbackContext) -> None:
+    keyboard = [
+        [
+            InlineKeyboardButton("Suomi", callback_data='fi'),
+            InlineKeyboardButton("Svenska", callback_data='sv'),
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Millä kielellä haluat lukea artikkeleita?', reply_markup=reply_markup)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+
+    query.answer()
+    
+    if(query.data == 'fi'):
+        language = 'suomi'
+    elif(query.data == 'sv'):
+        language = 'svenska'
+    else:
+        language = 'et valinnut kieltä'
+
+    query.edit_message_text(text=f"Valittu kieli: {language}")
+
 
 def moro(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Kato Sauli terve!")
@@ -41,6 +68,11 @@ def search(update, context):
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+
+updater.dispatcher.add_handler(CommandHandler('language', language))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
 
 moro_handler = CommandHandler('moro', moro)
 dispatcher.add_handler(moro_handler)
