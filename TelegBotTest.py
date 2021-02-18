c80b45a962134d66e3b0eb257066ca1b15c4a1e5
mariia.pyylampi@gmail.com <mariia.pyylampi@gmail.com>
2021-02-18 15:02:48 +0200

    add category & tag buttons and functionalities

--- TelegBotTest.py
+++ TelegBotTest.py
@@ -27,28 +27,29 @@ lista = []
 # functions
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hei, olen Ella-Botti! Voit hakea Elävän arkiston artikkeleita komennolla /hae [aihe]. Esimerkiksi koira-artikkeleita saat komennolla \"/hae koira\" Haku palauttaa 5 artikkelia")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hei, olen Ella-Botti! Voit hakea Elävän arkiston artikkeleita komennolla /hae_artikkeli [aihe]. Esimerkiksi koira-artikkeleita saat komennolla \"/hae_artikkeli koira\" Haku palauttaa 5 artikkelia")
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hej, jag är Boten-Ella. Du kan söka från Yle Arkivet med befallning /sok [tema]. Till exempel med \"/sok hund\" får du hundartiklar. Sök returnerar 5 artiklar" )
 
-def language(update: Update, context: CallbackContext) -> None:
+""" def language(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [
-            InlineKeyboardButton("Suomi", callback_data='fi'),
-            InlineKeyboardButton("Svenska", callback_data='se'),
+            InlineKeyboardButton("Suomi", callback_data='l1'),
+            InlineKeyboardButton("Svenska", callback_data='l2'),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('Millä kielellä haluat lukea artikkeleita?', reply_markup=reply_markup)
+    update.message.reply_text('Millä kielellä haluat lukea artikkeleita?', reply_markup=reply_markup) """
 
 
-
-
-
-def search(update, context, language):
-    
-    print(context.args)
-    search_word = context.args[0]
+def search(update, context, language, word):
+    if word:
+        search_word = word
+    elif not word: 
+        print(context.args)
+        search_word = context.args[0]
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="käytä hakusanaa")
 
     i = 0
     results = search_keyword(search_word,language)
@@ -70,40 +71,103 @@ def search(update, context, language):
 def show_more(update: Update, context : CallbackContext):
     keyboard = [
         [
-            InlineKeyboardButton("Näytä lisää", callback_data="s1")
+            InlineKeyboardButton("Näytä lisää", callback_data='s1')
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_text("Näytä lisää tuloksia ", reply_markup=reply_markup)
+
 def button(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
 
-
-
-    query.answer()
+    #query.answer()
     
-    if(query.data == 'fi'):
+    """   if(query.data == 'l1'):
         language = 'fi'
         search(context, update, language)
-    elif(query.data == 'se'):
-        language = 'se'
+    elif(query.data == 'l2'):
+        language = 'se' """
 
-    elif(query.data == 's1'):
+    if(query.data == 's1'):
         global lista
         language = lista[5:10]
-    else:
-        language = 'et valinnut kieltä'
+
 
 
     #search(update, context)
     query.edit_message_text(text=f"{language}")
+
+def category(update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton("Artikkelit", callback_data='c1'),
+            InlineKeyboardButton("Media", callback_data='c2'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Mitä etsit?', reply_markup=reply_markup)
+
+
+def handle_category(update, context):
+    query = update.callback_query
+
+    if query.data == 'c1':
+        articles_tag(update, context)
+        query.edit_message_text(text='Etsitään artikkeleita...')
+    elif query.data == 'c2':
+        media_tag(update, context)
+        query.edit_message_text(text='Etsitään mediaa...')
+
+
+def articles_tag(update, context):
+    keyboard = [
+            [
+                InlineKeyboardButton("Kotimaa", callback_data='a1'),
+                InlineKeyboardButton("Ulkomaat", callback_data='a2'),
+            ]
+        ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Mitä uutisia haluat lukea?', reply_markup=reply_markup)
+
+
+def handle_articles_tag(update, context):
+    query = update.callback_query
+
+    if query.data == 'a1':
+        print('kutsuu funktiota joka tietokannasta')
+        search(update, context, 'fi', 'kotimaa')
+        query.edit_message_text(text='Etsitään kotimaan uutisia...')
+    elif query.data == 'a2':
+        query.edit_message_text(text='Etsitään ulkomaiden uutisia...')
+
+
+def media_tag(update, context):
+    keyboard = [
+            [
+                InlineKeyboardButton("Radio", callback_data='m1'),
+                InlineKeyboardButton("TV", callback_data='m2'),
+            ]
+        ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Etsitkö radio- vai tv-ohjelmia?', reply_markup=reply_markup)
+
+
+def handle_media_tag(update, context):
+    query = update.callback_query
+
+    if query.data == 'm1':
+        query.edit_message_text(text='Etsitään radio-ohjelmia...')
+    elif query.data == 'm2':
+        query.edit_message_text(text='Etsitään tv-ohjelmia...')
+
+
     
-def hae(update, context):
-    search(update, context, "fi")
+def hae_artikkeli(update, context):
+    search(update, context, "fi", '')
 
 def sok(update, context):
-    search(update, context, "se")
+    search(update, context, "se", '')
 
 def moro(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Kato Sauli terve!")
@@ -129,15 +193,24 @@ def unknown(update, context):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.dispatcher.add_handler(CommandHandler('language', language))
+#updater.dispatcher.add_handler(CommandHandler('language', language))
 
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
+updater.dispatcher.add_handler(CallbackQueryHandler(button, pattern='s'))
 
-hae_handler = CommandHandler('hae', hae)
+hae_handler = CommandHandler('hae_artikkeli', hae_artikkeli)
 dispatcher.add_handler(hae_handler)
 
 sok_handler = CommandHandler('sok', sok)
 dispatcher.add_handler(sok_handler)
+
+category_handler = CommandHandler('hae', category)
+dispatcher.add_handler(category_handler)
+
+updater.dispatcher.add_handler(CallbackQueryHandler(handle_category, pattern='c'))
+
+updater.dispatcher.add_handler(CallbackQueryHandler(handle_articles_tag, pattern='a'))
+
+updater.dispatcher.add_handler(CallbackQueryHandler(handle_media_tag, pattern='m'))
 
 search_handler = CommandHandler('search', search)
 dispatcher.add_handler(search_handler)
