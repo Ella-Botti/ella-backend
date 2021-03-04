f1572a8d18325bdad45ed074f406f7b61de16e69
Samuli Peltonen <samuli.peltonen1@gmail.com>
2021-03-04 14:46:11 +0200

    Commented factOfTheDay.py

--- factOfTheDay.py
+++ factOfTheDay.py
@@ -7,10 +7,12 @@ def search_fact():
         data = json.load(json_file)
         todays_date = date.today()
         events = []
+        #compares all event dates and checks if the date matches with todays_date
         for key in data:
             if data[key]:
                 month = data[str(key)]['kuukausi']
                 day = data[str(key)]['paiva']
+                #if the dates match, append to events that have happened today
                 if month == str(todays_date.month) and day == str(todays_date.day):
                     event = f"Tänään {day}.{month}, vuonna {data[key]['vuosi']} {data[key]['tapahtuma']}"
                     events.append(event)
