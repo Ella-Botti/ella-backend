641b3d4f2b5af30ba07690ebc642c73e2b49e335
JaminaPPollanen <bgj428@myy.haaga-helia.fi>
2021-04-19 13:25:17 +0300

    reply to fact of the day

--- fact_of_the_day.py
+++ fact_of_the_day.py
@@ -1,7 +1,6 @@
 from datetime import date
 import psycopg2
-
-
+from random import randint
 
 
 def search_fact():
@@ -11,7 +10,13 @@ def search_fact():
 
     print("connection succesful")
 
-    #ajetaan tietokantakomento cursori.execute() metodilla.
+    # avataan vastausvaihtoehdot
+
+    content = ["Kävin läpi kenkälaatikkoani ja löysin tämän faktan...", "Pyyhkiessäni pölyjä komerossa tämä tippui päähäni...", "Sohvalla makoillessani tämä juolahti mieleeni...",
+               "Vietettyäni vuoden neljän seinän sisällä, muistelin tätä useasti...", "Vaaribot muisteli tätä eilen keinutuolissa...", "Tätä saattaa joskus tarvita tietovisassa...", "TRIVIA TIME!", "Tämä hiiren nakertama fakta löytyi mökiltä..."]
+    reply_number = randint(0, (len(content) - 1))
+
+    # ajetaan tietokantakomento cursori.execute() metodilla.
 
     todays_date = date.today()
     SQL = f"SELECT paiva, kuukausi, vuosi, tapahtuma FROM calendar WHERE kuukausi = {todays_date.month} AND paiva = {todays_date.day} ;"
@@ -20,14 +25,13 @@ def search_fact():
     print(f"Found {len(events)} events for today")
     print("Search succesful")
 
-    #tärkeää!
-    #suljetaan tietokantayhteys, sillä yhteyksiä voi olla vain rajallisesti!
+    # tärkeää!
+    # suljetaan tietokantayhteys, sillä yhteyksiä voi olla vain rajallisesti!
     conn.close()
 
     filtered_events = []
 
     for item in events:
-        event = f"Tänään {item[0]}.{item[1]}, vuonna {item[2]} {item[3]}"
+        event = f"{content[reply_number]} Tänään {item[0]}.{item[1]}, vuonna {item[2]} {item[3]}."
         filtered_events.append(event)
     return filtered_events
-    
